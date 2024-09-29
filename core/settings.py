5d16260abe35c515a43157fbded91e2e30229624
waflawe <popejpivaoleg@gmail.com>
2024-09-29 21:59:31 +0900

    Initial project core.

---
+++ core/settings.py
@@ -0,0 +1,135 @@
+from pathlib import Path
+from environ import Env
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = Env()
+Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(", ")
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'users.apps.UsersConfig'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
+    }
+}
+
+LOGGING = {
+    "version": 1,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler"
+        }
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": env("DJANGO_LOG_LEVEL", default="DEBUG")
+        }
+    },
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20,
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'eXwonder',
+    'DESCRIPTION': '',
+    'VERSION': '1.1.0',
+}
+
+AUTH_USER_MODEL = "users.ExwonderUser"
+
+DEFAULT_USER_TIMEZONE = "Europe/London"
+CUSTOM_USER_AVATARS_DIR = "avatars"
+DEFAULT_USER_AVATAR_PATH = "default-user-icon.jpg"
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles/"
+]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "mediafiles"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ users/models.py
@@ -0,0 +1,73 @@
+from typing import Optional
+
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import MinLengthValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+def get_uploaded_avatar_path(instance: Optional['ExwonderUser'] = None, filename: Optional[str] = None) -> str:
+    return f"{settings.CUSTOM_USER_AVATARS_DIR}/{filename}"
+
+
+class ExwonderUserManager(BaseUserManager):
+    def create_user(self, username: str, email: str, avatar: str, timezone: str, password: Optional[str] = None) \
+            -> 'ExwonderUser':
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user: 'ExwonderUser' = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            avatar=avatar,
+            timezone=timezone
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class ExwonderUser(AbstractBaseUser):
+    username = models.CharField(
+        verbose_name=_("Имя пользователя"),
+        max_length=16,
+        unique=True,
+        help_text=_("Не более 16 символов, не менее 5. Буквы, цифры, @/./+/-/_."),
+        validators=[UnicodeUsernameValidator(), MinLengthValidator(5)],
+        error_messages={
+            "unique": _("Пользователь с таким именем уже существует."),
+        }
+    )
+    email = models.EmailField(
+        verbose_name=_('Почта'),
+        unique=True,
+        help_text=_("Электронная почта для аккаунта.")
+    )
+    avatar = models.ImageField(
+        verbose_name=_("Аватарка"),
+        upload_to=get_uploaded_avatar_path,
+        default=settings.DEFAULT_USER_AVATAR_PATH
+    )
+    timezone = models.CharField(
+        verbose_name=_("Временная зона"),
+        max_length=64,
+        default=settings.DEFAULT_USER_TIMEZONE
+    )
+    date_joined = models.DateTimeField(verbose_name=_('Время регистрации'), auto_now_add=True)
+    is_active = models.BooleanField(verbose_name=_('Активен ли'), default=True)
+    is_2fa_enabled = models.BooleanField(verbose_name=_('Включена ли 2FA'), default=False)
+
+    USERNAME_FIELD = "username"
+    objects = ExwonderUserManager()
+
+    class Meta:
+        verbose_name = _("Пользователь")
+        verbose_name_plural = _("Пользователи")
+
+        db_table = "exwonder_users"
+
+    def __str__(self):
+        return f"{self.username}"
