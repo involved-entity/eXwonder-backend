4889688cc88ab0dc77254389732e4f0f369b598d
waflawe <popejpivaoleg@gmail.com>
2024-12-12 16:43:24 +0900

    Add authentication in notifications consumer

--- core/asgi.py
+++ core/asgi.py
@@ -1,6 +1,5 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -11,6 +10,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": URLRouter(websocket_urlpatterns),
     }
 )
--- notifications/consumers.py
+++ notifications/consumers.py
@@ -1,17 +1,36 @@
 import json
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class NotificationConsumer(AsyncWebsocketConsumer):
+    user_id: int | None
+    group_name: str | None
+
     async def connect(self):
-        self.group_name = f'user_{self.scope['user'].id}'  # noqa
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+        if self.group_name:
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        if data["type"] == "authenticate":
+            await self.authenticate(data["token"], data["user_id"])
 
     async def notify(self, event):
-        print(event["payload"])
         await self.send(text_data=json.dumps({"payload": event["payload"]}))
+
+    async def authenticate(self, token: str, user_id: int):
+        token = await database_sync_to_async(self.check_token)(token)
+        if token:
+            self.group_name = f"user_{user_id}"
+            self.user_id = user_id
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+
+    def check_token(self, token: str):
+        from rest_framework.authtoken.models import Token
+
+        return Token.objects.filter(key=token).exists()  # noqa
